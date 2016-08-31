42c75ea875b3376df40637795f375021fe75a98a
iOSlhy <ioslhy@qq.com>
2016-08-31 17:11:39 +0800

    ioslhy
    this is my first demo to the git

---
+++ HYOneMore/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HYOneMore
+//
+//  Created by 海洋 on 16/8/31.
+//  Copyright © 2016年 海洋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    NSLog(@"Hello World");
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
